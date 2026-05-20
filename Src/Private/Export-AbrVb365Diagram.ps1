298c28d3f2fbcb428cdf216170d5ff89ed81d030
Jonathan Colon <rebelinux@gmail.com>
2026-05-19 22:41:19 -0400

    Migrated to the New Veeam Icons for the Diagram

--- Src/Private/Export-AbrVb365Diagram.ps1
+++ Src/Private/Export-AbrVb365Diagram.ps1
@@ -28,26 +28,26 @@ function Export-AbrVb365Diagram {
 
             # Variable translating Icon to Image Path ($IconPath)
             $script:Images = @{
-                'VB365_Server' = 'VBR_server.png'
-                'VB365_Proxy_Server' = 'Proxy_Server.png'
-                'VB365_Proxy' = 'Veeam_Proxy.png'
+                'VB365_Server' = 'Server.png'
+                'VB365_Proxy_Server' = 'Server.png'
+                'VB365_Proxy' = 'Proxy.png'
                 'VBR_LOGO' = 'Veeam_logo_new.png'
                 'VB365_LOGO_Footer' = 'verified_recoverability.png'
                 'VB365_Repository' = 'VBO_Repository.png'
                 'VB365_Windows_Repository' = 'Windows_Repository.png'
-                'VB365_Object_Repository' = 'Object_Storage.png'
-                'VB365_Object_Support' = 'Object Storage support.png'
-                'Veeam_Repository' = 'Veeam_Repository.png'
-                'VB365_On_Premises' = 'SMB.png'
+                'VB365_Object_Repository' = 'Object_storage.png'
+                'VB365_Object_Support' = 'Object_storage.png'
+                'Veeam_Repository' = 'VBR_Repository.png'
+                'VB365_On_Premises' = 'Cluster.png'
                 'VB365_Microsoft_365' = 'Cloud.png'
-                'Microsoft_365' = 'Microsoft_365.png'
+                'Microsoft_365' = 'Microsoft_Office_365.png'
                 'Datacenter' = 'Datacenter.png'
                 'VB365_Restore_Portal' = 'Web_console.png'
                 'VB365_User_Group' = 'User_Group.png'
                 'VB365_User' = 'User.png'
-                'VBR365_Amazon_S3_Compatible' = 'S3-compatible.png'
-                'VBR365_Amazon_S3' = 'AWS S3.png'
-                'VBR365_Azure_Blob' = 'Azure Blob.png'
+                'VBR365_Amazon_S3_Compatible' = 'S3_compatible_Storage.png'
+                'VBR365_Amazon_S3' = 'AWS_S3.png'
+                'VBR365_Azure_Blob' = 'Microsoft_Azure_Blob_Storage.png'
             }
 
             $RootPath = Split-Path (Split-Path $PSScriptRoot -Parent) -Parent
